21b6ccc5bb381406dfbb9c292d142b5dbfb9bffb
n1rjal <nirjalpaudel54312@gmail.com>
2023-07-25 20:29:11 +0545

    all set for npx

---
+++ bin/index.js
@@ -0,0 +1,432 @@
+#! /usr/bin/env node
+
+/* The code is defining two constants: one for all typescript illegal characters and other for numeric types in js*/
+const TS_ILLEGAL_CHARACTERS = /[^\w\s\n]/g;
+const NUMERIC_TYPES = ["number", "bigint"];
+const {
+  readFileSync,
+  writeFile: writeFileFs,
+  existsSync,
+  mkdirSync,
+} = require("fs");
+const { join } = require("path");
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+/* The above code is defining an array called `ALL_DEFINED_ARGS` which contains objects representing
+command line arguments. Each object has properties such as `keys` (an array of possible argument
+keys), `required` (a boolean indicating if the argument is required), `question` (a string
+representing the question to ask the user when the argument is not provided), and `default` (a
+default value for the argument if not provided). These objects represent the different command line
+arguments that can be passed to a script or program. */
+const ALL_DEFINED_ARGS = [
+  { keys: ["-i", "--input"], required: true, question: "Postman input file" },
+  {
+    keys: ["-o", "--output"],
+    required: true,
+    question: "The output directory",
+  },
+  { keys: ["-ia", "--include-any"], required: false, default: false },
+  {
+    keys: ["-sc", "--status-codes"],
+    required: false,
+    default: "",
+  },
+  {
+    keys: ["-ft", "--force-text"],
+    required: false,
+    default: false,
+  },
+  {
+    keys: ["-te", "--throw-error"],
+    required: false,
+    default: false,
+  },
+];
+
+/* The above code is defining a map called `ARGS_HELP_MAP` which is used to store help messages for
+different command line arguments. Each argument is associated with a help message that describes its
+purpose. The map is populated with key-value pairs where the key is the command line argument and
+the value is the corresponding help message. */
+const ARGS_HELP_MAP = new Map();
+// from the key array we will be using the first one
+// and ignoring the last one
+ARGS_HELP_MAP.set(
+  ALL_DEFINED_ARGS[0],
+  `The export of postman data as json v2.0`
+);
+ARGS_HELP_MAP.set(ALL_DEFINED_ARGS[1], `The output directory for all types`);
+ARGS_HELP_MAP.set(
+  ALL_DEFINED_ARGS[2],
+  `This specifies if any must be included in typescript types or not. Default value is false`
+);
+ARGS_HELP_MAP.set(
+  ALL_DEFINED_ARGS[3],
+  `This specifies if we can add type support for response of the types provided. Default value is 200[comma separated]`
+);
+ARGS_HELP_MAP.set(
+  ALL_DEFINED_ARGS[4],
+  `This specifies if we need to parse text content in postman request or not. Default value is false`
+);
+ARGS_HELP_MAP.set(
+  ALL_DEFINED_ARGS[5],
+  `This specifies if the program should throw error`
+);
+
+/* The above code is filtering an array called `ALL_DEFINED_ARGS` to only include elements that have a
+property `required` set to `true`. */
+let requiredArgs = ALL_DEFINED_ARGS.filter((arg) => arg.required);
+
+/* The above code is declaring and initializing several variables. */
+let file = "";
+let throwError = false;
+let INCLUDE_ANY = true;
+let TYPES_FOR_STATUS_CODE = [200];
+let OUTPUT_DIR = "";
+let PARSE_TYPES = ["json"];
+
+/**
+ * The function `printHelp` checks if the user has requested help by checking command line arguments,
+ * and if so, it prints out information about available options and their descriptions.
+ * @returns `false` if the user did not ask for help, and it is not returning anything if the user
+ * asked for help.
+ */
+function printHelp() {
+  const userAskedForHelp =
+    process.argv.includes("-h") || process.argv.includes("--help");
+
+  if (!userAskedForHelp) return false;
+
+  console.log(`\n`);
+  for (const [argKey, argHelp] of ARGS_HELP_MAP.entries()) {
+    const keys = argKey.keys;
+    console.log(`Option Flag: ${keys.join(", ")}`);
+    if (argHelp.required)
+      console.log(`Required: ${argHelp.required.toString()}`);
+    if (!argKey.required && argHelp.default)
+      console.log(`Default: ${argHelp.default.toString()}`);
+    console.log(`${argHelp}\n\n`);
+  }
+  process.exit(0);
+}
+
+/**
+ * The function `parseAllRequiredArgs` parses command line arguments and checks if all required
+ * arguments are present.
+ * @returns an object containing the parsed command line arguments.
+ */
+async function parseArgs() {
+  // we will keep iterating and popping from the array until it is empty
+  // if it is not empty we say that the command line arguments are invalid
+  // and the following are required or we can ask for user input
+  // the args here is raw meaning it will have -h and -sc as the key of object args
+  const args = {};
+
+  for (let i = 2; i < process.argv.length; i++) {
+    const argKey = process.argv[i];
+    args[argKey] = process.argv[++i];
+
+    // basically we are searching by both keys
+    requiredArgs = requiredArgs.filter(
+      (arg) => !(arg.keys[0] === argKey || arg.keys[1] === argKey)
+    );
+  }
+
+  // fill in the rest with defaults
+  for (const arg of ALL_DEFINED_ARGS) {
+    if (!args[arg.keys[0]]) {
+      args[arg.keys[0]] = arg.default;
+    }
+  }
+
+  // prompt user for required arguments
+  if (requiredArgs.length) {
+    for (const arg of requiredArgs) {
+      args[arg.keys[0]] = await askQuestion(arg.question);
+    }
+  }
+
+  return args;
+}
+
+/**
+ * The function `askQuestion` is an asynchronous function that prompts the user with a question and
+ * returns a promise that resolves with the user's answer.
+ * @param questionText - The `questionText` parameter is a string that represents the text of the
+ * question you want to ask the user.
+ * @returns The function `askQuestion` returns a promise.
+ */
+async function askQuestion(questionText) {
+  return new Promise((resolve, reject) => {
+    const question = `# ${questionText}: `;
+    rl.question(question, (answer) => {
+      resolve(answer);
+    });
+  });
+}
+
+/**
+ * The function `writeFile` is a JavaScript function that writes content to a file and returns a
+ * promise that resolves to `true` if the operation is successful.
+ * @param filePath - The `filePath` parameter is a string that represents the path to the file where
+ * you want to write the content. It should include the file name and extension.
+ * @param fileContent - The `fileContent` parameter is the content that you want to write to the file.
+ * It can be a string, an object, or any other data type that can be converted to a string.
+ * @returns a Promise object.
+ */
+function writeFile(filePath, fileContent) {
+  return new Promise((resolve, reject) => {
+    writeFileFs(filePath, fileContent, { flag: "w+" }, (err) => {
+      if (err) reject(err);
+      resolve(true);
+    });
+  });
+}
+
+/**
+ * The function converts a string to camel case by removing illegal characters, splitting the string
+ * into words, and capitalizing the first letter of each word except the first one.
+ * @param string - The `string` parameter is the input string that you want to convert to camel case.
+ * @returns a camelCase version of the input string.
+ */
+function toCamelCase(string) {
+  const legalString = string
+    .replaceAll("\n", "")
+    .replaceAll("-", "_")
+    .replaceAll(TS_ILLEGAL_CHARACTERS, "")
+    .split(" ")
+    .map((words) => words.trim())
+
+    .filter((string) => string.length > 0);
+
+  return legalString.reduce((accumulator, word) => {
+    return accumulator + word[0].toUpperCase() + word.slice(1);
+  }, "");
+}
+
+/**
+ * The function `parseTypeFromPostManRequestQuery` takes in a name and a request object, and if the
+ * request has a query, it converts the query parameters into a TypeScript type and writes it to a
+ * file.
+ * @param name - The `name` parameter is a string that represents the name of the request or query. It
+ * is used to generate the file name and the interface name in the output file.
+ * @param request - The `request` parameter is an object that represents a request made in Postman. It
+ * contains information about the request, such as the method (e.g., GET, POST) and the URL.
+ */
+async function parseTypeFromPostManRequestQuery(name, request) {
+  if (request?.url?.query && request?.url?.query?.length > 0) {
+    const method = request.method;
+    const url = request.url.raw;
+
+    if (request?.url?.query && request?.url?.query.length > 0) {
+      const json = request.url.query.reduce(
+        (accumulator, item) => ({
+          ...accumulator,
+          [TS_ILLEGAL_CHARACTERS.test(item.key) ? `"${item.key}"` : item.key]:
+            item.value,
+        }),
+        {}
+      );
+      const content = getTypeScriptTypeFromRawJson(name, JSON.stringify(json));
+      const path = join(
+        `${OUTPUT_DIR}`,
+        "queries",
+        `I${toCamelCase(name)}Query.ts`
+      );
+      const fileContent = `/*\n${name}\n${method}: ${url}\n*/\n` + content;
+      writeFile(path, fileContent, { flag: "w+" }, (err) => {
+        if (err) throw err;
+      });
+    }
+  }
+}
+
+/**
+ * The function `parseTypeFromPostManJsonBody` parses a JSON body from a Postman request and generates
+ * a TypeScript type definition file based on the parsed data.
+ * @param req - The `req` parameter is an object that represents the request made to the server. It
+ * contains information such as the request method (`req.method`), the request URL (`req.url.raw`), and
+ * the query parameters (`req.url.query`).
+ */
+async function parseTypeFromPostManRequestJsonBody(name, req) {
+  if (req?.body?.options?.raw?.language === "json" && req?.body?.raw) {
+    const raw = req.body.raw;
+    const method = req.method;
+    const url = req?.url?.raw ?? req.url;
+
+    const content = getTypeScriptTypeFromRawJson(name, raw);
+    const path = join(
+      `${OUTPUT_DIR}`,
+      "request",
+      `I${toCamelCase(name)}Request.ts`
+    );
+    const fileContent = `/*\n${name}\n${method}: ${url}\n*/\n` + content;
+    return writeFile(path, fileContent);
+  }
+}
+
+/**
+ * The function `parseTypesFromPostManExamples` parses types from a Postman response and generates a
+ * TypeScript interface file.
+ * @param resp - The `resp` parameter is an object that represents a response from a Postman request.
+ * It contains the following properties:
+ */
+async function parseTypesFromPostManExampleResponse(resp) {
+  const name = `${resp.name} ResponseBody`;
+  const rawJson = resp.body;
+
+  if (
+    PARSE_TYPES.includes(resp._postman_previewlanguage) &&
+    rawJson.length > 0 &&
+    TYPES_FOR_STATUS_CODE.includes(200)
+  ) {
+    const content = getTypeScriptTypeFromRawJson(name, rawJson);
+    const fileContent = `/*\n${name}\n*/\n` + content;
+    const path = join(`${OUTPUT_DIR}`, "response", `I${toCamelCase(name)}.ts`);
+    return writeFile(path, fileContent);
+  }
+}
+
+/**
+ * The function `getTypescriptEquivalentForVariable` returns the TypeScript equivalent type for a given
+ * JavaScript variable.
+ * @param value - The `value` parameter in the `getTypescriptEquivalentForVariable` function is the
+ * variable for which we want to determine the TypeScript equivalent type.
+ * @returns a TypeScript equivalent type for the given variable.
+ */
+function getTypescriptEquivalentForVariable(value) {
+  const type = typeof value;
+
+  if (type === "object") {
+    if (Array.isArray(value)) {
+      const type = getTypescriptEquivalentForVariable(value[0]);
+      return type ? `${type}[]` : INCLUDE_ANY ? "any[]" : "unknown[]";
+    }
+    let nestedType = getTypeScriptTypeFromRawJson(null, JSON.stringify(value));
+    return nestedType;
+  }
+
+  if (type === "boolean") return "boolean";
+  if (NUMERIC_TYPES.includes(type)) return "number";
+  if (type === "string") return "string";
+  if (value === null) return "null";
+  return INCLUDE_ANY ? "any" : "unknown";
+}
+
+/**
+ * The function `getTypeScriptTypeFromRawJson` takes a name and a raw JSON string as input and returns
+ * a TypeScript interface or type definition based on the structure of the JSON data.
+ * @param name - The `name` parameter is a string that represents the name of the TypeScript interface
+ * you want to generate. It is used to create the interface name by converting it to camel case and
+ * prefixing it with "I".
+ * @param rawJson - The `rawJson` parameter is a string that represents a JSON object or array. It is
+ * the raw JSON data that you want to convert into a TypeScript type.
+ * @returns a TypeScript interface or object literal as a string.
+ */
+function getTypeScriptTypeFromRawJson(name, rawJson) {
+  let response =
+    name.length > 0 ? `export interface I${toCamelCase(name)} { \n` : "{";
+  try {
+    let jsonData = JSON.parse(rawJson);
+    if (Array.isArray(jsonData) && jsonData.length > 0) jsonData = jsonData[0];
+
+    for (const key in jsonData) {
+      response += `  ${key}: ${getTypescriptEquivalentForVariable(
+        jsonData[key]
+      )}; \n`;
+    }
+    response += "}";
+    return response;
+  } catch (e) {
+    if (throwError) throw e;
+  }
+}
+
+/**
+ * The function `getData` processes JSON data and generates TypeScript type definitions based on the
+ * data structure.
+ * @param jsonData - The `jsonData` parameter is an object that contains information about a request
+ * and its corresponding response. It is expected to have the following properties:
+ */
+async function getData(jsonData) {
+  if (jsonData.item) {
+    for (const item of jsonData.item) {
+      await getData(item);
+    }
+  } else {
+    const req = jsonData.request;
+    const exampleResponses = jsonData.response;
+    await parseTypeFromPostManRequestJsonBody(jsonData.name, req);
+    await parseTypeFromPostManRequestQuery(jsonData.name, req);
+    await Promise.all(
+      exampleResponses.map(async (resp) =>
+        parseTypesFromPostManExampleResponse(resp)
+      )
+    );
+    return true;
+  }
+}
+
+/**
+ * The main function reads command line arguments, checks file and output folder existence, creates
+ * necessary directories, reads JSON data from a file, and calls the getData function.
+ */
+async function main() {
+  const args = await parseArgs();
+  file = args["-i"];
+  throwError = Boolean(args["-te"]);
+  INCLUDE_ANY = Boolean(args["-ia"]);
+  OUTPUT_DIR = args["-o"];
+  PARSE_TYPES = ["json"];
+
+  if (args["-ft"]) PARSE_TYPES.push("text");
+
+  if (args["-sc"] && args?.["-sc"]?.split(",").length > 0)
+    args?.sc?.split(",").forEach((code) => {
+      TYPES_FOR_STATUS_CODE.push(parseInt(code));
+    });
+
+  const postManFileExists = existsSync(file);
+  const outputFolderExists = existsSync(OUTPUT_DIR);
+
+  if (!postManFileExists) {
+    console.log(`File ${file} does not exist`);
+    process.exit(1);
+  }
+
+  if (!outputFolderExists) {
+    mkdirSync(OUTPUT_DIR);
+  }
+
+  if (!existsSync(join(OUTPUT_DIR, "queries"))) {
+    mkdirSync(join(OUTPUT_DIR, "queries"));
+  }
+
+  if (!existsSync(join(OUTPUT_DIR, "request"))) {
+    mkdirSync(join(OUTPUT_DIR, "request"));
+  }
+
+  if (!existsSync(join(OUTPUT_DIR, "response"))) {
+    mkdirSync(join(OUTPUT_DIR, "response"));
+  }
+
+  const data = readFileSync(file);
+  const jsonData = JSON.parse(data);
+  await getData(jsonData);
+}
+
+// if user has asked for help this will print the help
+// help is asked if -h or --help is present in command
+printHelp();
+
+// run the main function
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    if (throwError) console.log(e?.message ?? e);
+    process.exit(1);
+  });
